8cb62be3ba34095974c3b4e9da9f88d12c8c4043
razzaksr <razzaksr@gmail.com>
2024-10-14 15:31:22 +0530

    changes 5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from Zealous!!!
+          Learn React from Zealous Tech Corp!!!
         </a>
       </header>
     </div>
